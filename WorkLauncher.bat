d4c3e5231fca84f1e9c4470aca32d2505ffd4402
Priyesh <priyesh.dkr@gmail.com>
2020-01-17 17:07:24 +0530

    bla bla bla

---
+++ WorkLauncher.bat
@@ -0,0 +1,18 @@
+@echo off
+title Launcher
+echo Loading...
+REM Open WebPages...
+start "" "https://mail.google.com/mail/"
+start "" "https://news.google.com/"
+start "" "https://www.udemy.com/"
+start "" "https://read.amazon.com/"
+start "" "https://markmanson.net/"
+REM Open Hyper terminal...
+start "" "C:\Users\g9oohp\AppData\Local\hyper\Hyper.exe" "D:\workdir"
+REM Open VS Code...
+start "" "C:\Users\g9oohp\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+REM Open Office mail...
+start "" "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Microsoft Office 2013\Outlook 2013.lnk"
+REM Open Microsoft Teams
+start "" "C:\Users\g9oohp\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Microsoft Corporation\Microsoft Teams.lnk"
+exit
